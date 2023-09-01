e15f1f97cb5ff2b811abc8e1957ddcfef5e78ac8
mereo83 <mereo83@gmail.com>
2023-09-01 23:21:26 +0400

    modified app.js

--- app.js
+++ app.js
@@ -4,10 +4,59 @@ app.use(express.json()); // Add this line to parse JSON request bodies
 const port = 3000;
 
 const books = [
-  { id: 1, Bkname: 'Things fall apart', author: 'Achebe' },
-  { id: 2, Bkname: 'Rom n Julie', author: 'Shakespeare' },
-  { id: 3, Bkname: 'Unoma', author: 'Moi' }
+
+  {
+    bookId: 1,
+    book_name: "Alice's Adventures in Wonderland",
+    author: "Lewis Carroll"
+  },
+  {
+    bookId: 2,
+    book_name: "The Little Mermaid",
+    author: "Hans Christian Andersen"
+  },
+  {
+    bookId: 3,
+    book_name: "Winnie-the-Pooh",
+    author: "A.A. Milne"
+  },
+  {
+    bookId: 4,
+    book_name: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+    author: "C.S. Lewis"
+  },
+  {
+    bookId: 5,
+    book_name: "Peter Pan",
+    author: "J.M. Barrie"
+  },
+  {
+    bookId: 6,
+    book_name: "Matilda",
+    author: "Roald Dahl"
+  },
+  {
+    bookId: 7,
+    book_name: "Where the Wild Things Are",
+    author: "Maurice Sendak"
+  },
+  {
+    bookId: 8,
+    book_name: "Charlotte's Web",
+    author: "E.B. White"
+  },
+  {
+    bookId: 9,
+    book_name: "The Gruffalo",
+    author: "Julia Donaldson"
+  },
+  {
+    bookId: 10,
+    book_name: "Goodnight Moon",
+    author: "Margaret Wise Brown"
+  }
 ];
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to my book store');
